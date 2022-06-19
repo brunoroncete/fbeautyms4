b155164cac9f5192031ea3eb1b86bd1a5995ebec
brunoroncete <brunoroncete@hotmail.com>
2022-06-19 15:50:22 +0000

    added products page

--- products/models.py
+++ products/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 
 class Category(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
