874286063757af9c0acfa5e9660b2b3c93467553
brunoroncete <brunoroncete@hotmail.com>
2022-07-31 16:46:48 +0000

    setup aws cache control

--- foxybeauty/settings.py
+++ foxybeauty/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['foxybeautyms4.herokuapp.com', 'localhost']
 
@@ -179,6 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fbeautyms4'
     AWS_S3_REGION_NAME = 'eu-west-2'
