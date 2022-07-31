75a4ab61ea6350e97bb000ec58708f209deb7957
brunoroncete <brunoroncete@hotmail.com>
2022-07-31 12:46:08 +0000

    deployment part2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn foxybeauty.wsgi:application
--- foxybeauty/settings.py
+++ foxybeauty/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foxybeautyms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -112,6 +112,7 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 WSGI_APPLICATION = 'foxybeauty.wsgi.application'
 
@@ -119,12 +120,17 @@ WSGI_APPLICATION = 'foxybeauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 #DATABASES = {
 #    'default': dj_database_url.parse('postgres://aoxzqrspgtpwvd:b2e2ad3d31fe675fd1e9ff5b23c12ef184cdd53f87d84190c8faea184b9c2b5f@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/d9kee95s3utbod')
