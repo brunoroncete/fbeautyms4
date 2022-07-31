18062eda7e385db01b6bafe3b79de6ae0bf7ee93
brunoroncete <brunoroncete@hotmail.com>
2022-07-31 15:26:37 +0000

    fixing deployment issue

--- foxybeauty/settings.py
+++ foxybeauty/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT'
+DEBUG = True
 
 ALLOWED_HOSTS = ['foxybeautyms4.herokuapp.com', 'localhost']
-
 
 # Application definition
 
